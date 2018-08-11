ea5624b5549fd4b72ea704ee8fda340d9a1d5e53
Brian Alexander <balexander04@gmail.com>
2018-08-11 11:04:24 -0500

    I think I have implemented async function to improve performance.

--- renameImages.py
+++ renameImages.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import sys
 import logging
@@ -5,54 +6,51 @@ from datetime import datetime
 from typing import Generator
 
 from PIL import Image
-# import pytesseract
 
 
 logging.captureWarnings(True)
 logging.basicConfig(
     level=logging.DEBUG,
-    format="%(levelname)7s: %(message)s",
+    format='%(levelname)7s: %(message)s',
     stream=sys.stdout,
 )
 
-LOG = logging.getLogger("")
+LOG = logging.getLogger('')
 
 
 def format_timestamp(timestamp) -> str:
-    """
-        Format timestamp to be a file name friendly timestamp
-    """
-    new_fmt = "%Y-%m-%d-%H-%M-%S"
-    original_fmt = "%Y:%m:%d %H:%M:%S"
+    """Format timestamp to be a file name friendly timestamp"""
+    new_fmt = '%Y-%m-%d-%H-%M-%S'
+    original_fmt = '%Y:%m:%d %H:%M:%S'
     return datetime.strptime(timestamp, original_fmt).strftime(new_fmt)
 
 
 def get_image_timestamp(image_file) -> str:
-    """
-        Return timestamp from the exif data from image_file
-    """
+    """Return timestamp from the exif data from image_file"""
     with Image.open(image_file) as img:
         return img._getexif()[36867]
 
 
 def rename_img_file(source, destination, skip_dupes=False):
+    """Using os.rename method to update image filename to timestamp format"""
     if not os.path.isfile(destination):
         LOG.info(f"{source} --> {destination}")
         # uncomment when for sure ready to run
         # os.rename(source, destination)
     elif skip_dupes:
         LOG.info(
-            f"SKIPPING - Some sort of duplicate {source} **** {destination}"
+            f'SKIPPING - Some sort of duplicate {source} **** {destination}'
         )
     else:
-        LOG.info(f"Some sort of duplicate {source} **** {destination}")
-        os.rename(source, destination.replace(".JPG", "-duplicate.JPG"))
+        LOG.info(f'Some sort of duplicate {source} **** {destination}')
+        os.rename(source, destination.replace('.JPG', '-duplicate.JPG'))
 
 
 def get_list_of_images(photo_dir) -> Generator:
-    """
-        Returns a list of images for the given directory
-        Note: There is no verification that the files are images
+    """Returns a list of images for the given directory
+
+    Note: There is no verification that the files are images however
+    .DS_Store files are filtered out.
     """
     for img in os.listdir(photo_dir):
         if os.path.isfile(os.path.join(photo_dir, img)) and img not in (
@@ -61,25 +59,34 @@ def get_list_of_images(photo_dir) -> Generator:
             yield img
 
 
-if __name__ == "__main__":
+async def process_image(directory_path, image):
+    """Take file name and path to process image"""
+    img_file_path = os.path.join(directory_path, image)
+    file_ext = image.split('.')[1]
+    img_timestamp = get_image_timestamp(img_file_path)
+    new_file_name = f'{format_timestamp(img_timestamp)}.{file_ext}'
+    new_file_path = os.path.join(PHOTO_DIRECTORY, new_file_name)
+    rename_img_file(img_file_path, new_file_path)
+
+
+if __name__ == '__main__':
     start_time = datetime.now()
-    home_directory = ""
+    home_directory = ''
     try:
-        PHOTO_DIRECTORY = f"{home_directory}/Desktop/{sys.argv[1]}"
+        PHOTO_DIRECTORY = f'{home_directory}/Desktop/{sys.argv[1]}'
     except IndexError as e:
-        PHOTO_DIRECTORY = f"{home_directory}~/Desktop/field"
+        raise Exception('No command line arg passed')
 
-    images_renamed = []
-
-    for img_file in get_list_of_images(PHOTO_DIRECTORY):
-        img_file_path = os.path.join(PHOTO_DIRECTORY, img_file)
-        file_ext = img_file.split('.')[1]
-        if os.path.isfile(img_file_path):
-            img_timestamp = get_image_timestamp(img_file_path)
-            new_file_name = f"{format_timestamp(img_timestamp)}.{file_ext}"
-            new_file_path = os.path.join(PHOTO_DIRECTORY, new_file_name)
-            rename_img_file(img_file_path, new_file_path)
-            images_renamed.append(img_timestamp)
+    loop = asyncio.get_event_loop()
+    future = asyncio.Future()
+    tasks = [
+        asyncio.ensure_future(process_image(PHOTO_DIRECTORY, img_file))
+        for img_file in get_list_of_images(PHOTO_DIRECTORY)
+        if os.path.isfile(os.path.join(PHOTO_DIRECTORY, img_file))
+    ]
+    loop.run_until_complete(
+        asyncio.gather(*tasks, return_exceptions=True)
+    )
 
     duration = datetime.now() - start_time
-    LOG.info(f"Total Time: {duration}")
+    LOG.info(f'Total Time: {duration}')
